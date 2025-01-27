520de9d171d404e265a8325fd9f61a352a51a7a1
lno182 <leonardounit@hotmail.com>
2025-01-27 16:06:41 +0000

    adicionando soma

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,7 +8,6 @@
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    int soma ,  divisao , multiplicacao, subtracao;
 
     float populacao; // população da cidade
     float area; //area em km²
@@ -21,6 +20,9 @@ int main() {
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
     printf("***Insira os daods da sua carta***\n");//titulo 
+    printf("A01   Brasil \n");
+    printf("Cidade: São paulo\n");
+
 
     printf("Entre com a pupulção da cidade: ");//entrada da população da cidade 
     scanf("%f", &populacao);
@@ -31,14 +33,18 @@ int main() {
     printf("Entre com o PIB da cidade: ");//entrada do PIB em R$
     scanf("%f", &pib);
     
-    printf("Entre com os pontos turisticos da cidade: ");
+    printf("Entre com os pontos turisticos da cidade: ");// entrada dos pontos turisticos
     scanf("%d", &turisticos);
+
+    float soma = populacao + area + pib;
+
 
 
     printf("População: %f\n", populacao);
     printf("Àrea: %f Km²\n", area);
     printf("PIB: R$ %f\n", pib);
     printf("Pontos Turisticos: %d\n", turisticos);
+    printf("Super poder: %f\n", soma);
     //printf("Entre como o nome da ")
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
